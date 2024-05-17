74e4e4339037647334eced9d0e5aa301f3e8c706
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-17 01:38:29 -0500

    bcryptjs encriptacion - hook beforeCreate en User.js encriptar password

--- src/models/User.js
+++ src/models/User.js
@@ -1,28 +1,39 @@
 import { DataTypes } from "sequelize";
 import db from "../config/db.js";
+import bcrypt from "bcryptjs";
 
-const User = db.define("users", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        require: true,
+const User = db.define(
+    "users",
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            require: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            require: true,
+            trim: true,
+        },
+        token: {
+            type: DataTypes.STRING,
+            require: false,
+        },
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        require: true,
-        unique: true,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        require: true,
-        trim: true,
-    },
-    token: {
-        type: DataTypes.STRING,
-        require: false,
-    },
-});
+    {
+        hooks: {
+            beforeCreate: async (user) => {
+                user.password = await bcrypt.hash(user.password, 10);
+            },
+        },
+    }
+);
 
 export default User;
