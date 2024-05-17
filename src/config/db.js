5274ed6106f2399b798a6d79224ad1c6c6504493
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-16 23:26:44 -0500

    Configuracion a la base y extructura del proyecto

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+
+const db = new Sequelize("products", "root", "", {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    timezone: "America/Bogota",
+    define: {
+        timestamps: true,
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 1000,
+    },
+});
+
+export default db;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import db from "./config/db.js";
 
 const app = express();
+
+//Conexion BDD
+try {
+    db.authenticate();
+    db.sync();
+    console.log("Conexion correcta a la BDD");
+} catch (error) {
+    console.log("error en la base");
+}
 
 const port = 8000;
 
